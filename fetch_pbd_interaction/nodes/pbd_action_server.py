6bd92bdee2b6c970e59b38fad1ed90ee6f59b654
ruffsl <roxfoxpox@gmail.com>
2017-03-03 22:03:05 -0800

    Sort of working need to debug continue_on_failure

--- fetch_pbd_interaction/nodes/pbd_action_server.py
+++ fetch_pbd_interaction/nodes/pbd_action_server.py
@@ -48,17 +48,18 @@ class PBDAction(object):
 
         # Run the system
         self.tf_listener = TransformListener()
-        self.robot = Robot(tf_listener)
+        self.robot = Robot(self.tf_listener)
         self.im_server = InteractiveMarkerServer(TOPIC_IM_SERVER)
         # Get path to example json file
         file_path = rospy.get_param("from_file")
-        self.session = Session(robot, tf_listener,
-                      im_server, from_file=file_path)
+        self.session = Session(self.robot, self.tf_listener,
+                      self.im_server, from_file=file_path)
 
-        if session.n_actions() > 0:
-            rospy.loginfo("There are {} actions.".format(session.n_actions()))
+        n_actions = self.session.n_actions()
+        if n_actions > 0:
+            rospy.loginfo("There are {} actions.".format(n_actions))
 
-        self.action_server = actionlib.SimpleActionServer(
+        self.action_server = SimpleActionServer(
             self.action_server_name,
             ExecuteAction,
             auto_start=False)
@@ -73,74 +74,73 @@ class PBDAction(object):
         self.execute(target_goal)
 
     def execute(self, target_goal):
-        if self.session.switch_to_action_by_name(target_goal.action_name):
-            n_primitives = self.session.n_primitives()
-            rospy.loginfo("Executing action %s, " +
-                          "with {} primitives.".format(
-                          target_goal.action_name, n_primitives))
-            # if self.session.execute_current_action():
-            #     self.action_server.set_succeeded()
+        action_names = target_goal.action_names
+        continue_on_failure = target_goal.continue_on_failure
+        actions_completed = [False] * len(action_names)
+        success = True
+        for index, action_name in enumerate(action_names):
+            switched = self.session.switch_to_action_by_name(action_name)
+            if switched:
+                completed = not self.session.execute_current_action()
+                actions_completed[index] = completed
 
-            primitives_executed = 0
-            for primitive in range(n_primitives):
-                if self.session.execute_primitive(primitive):
-                    self.send_feedback(target_goal, n_primitives, primitive)
-                    primitives_executed += 1
-            self.send_result(target_goal, n_primitives, primitives_executed)
-        else:
-            self.send_abort(target_goal, 0, 0)
-            rospy.logwarn("No actions available!")
+                if completed:
+                    self.send_feedback(target_goal, action_name, index, actions_completed)
+                elif continue_on_failure:
+                    success = False
+                    rospy.logwarn(("Action {} not completed! " +
+                                  "Dazed and confused, but trying to continue").format(action_name))
+                else:
+                    success = False
+                    self.send_abort(target_goal, actions_completed, success)
+                    rospy.logwarn(("Action {} not completed! " +
+                                  "Aborting now.").format(action_name))
+                    return
 
-    def send_feedback(self, target_goal, n_primitives, primitive_executed):
+            elif continue_on_failure:
+                success = False
+                rospy.logwarn(("No action {} available! " +
+                              "Dazed and confused, but trying to continue").format(action_name))
+            else:
+                success = False
+                self.send_abort(target_goal, actions_completed, success)
+                rospy.logwarn(("No action {} available! " +
+                              "Aborting now.").format(action_name))
+        self.send_result(target_goal, actions_completed, success)
+
+    def send_feedback(self, target_goal,
+                            current_action_executed_name,
+                            current_action_executed_index,
+                            current_actions_completed):
         """ Call this method to send feedback about the progress of the action """
 
         feedback = ExecuteFeedback()
-        feedback.action_name = target_goal.action_name
-        feedback.n_primitives = n_primitives
-        feedback.primitive_executed = primitive_executed
+        feedback.action_names = target_goal.action_names
+        feedback.current_action_executed_name = current_action_executed_name
+        feedback.current_action_executed_index = current_action_executed_index
+        feedback.current_actions_completed = current_actions_completed
         self.action_server.publish_feedback(feedback)
-        rospy.loginfo("Execute feedback. action_name: {0} " +
-                        "n_primitives: {1} " +
-                        "primitive_executed: {2}".format(
-                            feedback.action_name,
-                            feedback.n_primitives,
-                            feedback.primitive_executed))
+        rospy.loginfo("Execute feedback.\n{0}".format(str(feedback)))
 
-    def send_result(self, target_goal, n_primitives, primitives_executed):
+    def send_result(self, target_goal, actions_completed, success):
         """ Call this method to send resutls about of the action """
 
         result = ExecuteResult()
-        result.action_name = target_goal.action_name
-        result.n_primitives = n_primitives
-        result.primitives_executed = primitives_executed
-        result.action_completed = n_primitives == primitives_executed
-        self.action_server.set_succeeded(feedback)
-        rospy.loginfo("Execute result. action_name: {0} " +
-                        "n_primitives: {1} " +
-                        "primitives_executed: {2} " +
-                        "action_completed: {3}".format(
-                            result.action_name,
-                            result.n_primitives,
-                            result.primitives_executed,
-                            result.action_completed))
+        result.action_names = target_goal.action_names
+        result.actions_completed = actions_completed
+        result.success = success
+        self.action_server.set_succeeded(result)
+        rospy.loginfo("Execute result.\n{0}".format(str(result)))
 
-    def send_abort(self, target_goal, n_primitives, primitives_executed):
+    def send_abort(self, target_goal, actions_completed, success):
         """ Call this method to send abort info about of the action """
 
         result = ExecuteResult()
-        result.action_name = target_goal.action_name
-        result.n_primitives = n_primitives
-        result.primitives_executed = primitives_executed
-        result.action_completed = False
-        self.action_server.set_aborted(feedback)
-        rospy.loginfo("Execute abort. action_name: {0} " +
-                        "n_primitives: {1} " +
-                        "primitives_executed: {2} " +
-                        "action_completed: {3}".format(
-                            result.action_name,
-                            result.n_primitives,
-                            result.primitives_executed,
-                            result.action_completed))
+        result.action_names = target_goal.action_names
+        result.actions_completed = actions_completed
+        result.success = success
+        self.action_server.set_aborted(result)
+        rospy.loginfo("Execute abort.\n{0}".format(str(result)))
 
 # ######################################################################
 # Main Function
@@ -148,5 +148,6 @@ class PBDAction(object):
 
 if __name__ == '__main__':
     rospy.init_node('fetch_pbd_action_server')
-    server = PBDAction(rospy.get_name()))
+    server = PBDAction('/pbd_action_exicute')
+    server.start()
     rospy.spin()
