3755b4376629b0d29b632d7f3bec62c0f6b6bf74
ruffsl <roxfoxpox@gmail.com>
2017-03-14 12:19:47 -0700

    Making demo client a class

--- fetch_pbd_interaction/nodes/pbd_action_demo.py
+++ fetch_pbd_interaction/nodes/pbd_action_demo.py
@@ -21,37 +21,38 @@ import fetch_pbd_interaction.msg
 # Module level constants
 # ######################################################################
 
-action_server_name = 'pbd_action_exicute'
+ACTION_SERVER_NAME = 'pbd_action_exicute'
 
-def PBDClient():
-    # Creates the SimpleActionClient, passing the type of the action
-    client = actionlib.SimpleActionClient(
-        action_server_name,
-        fetch_pbd_interaction.msg.ExecuteAction
-    )
+class PBDClient():
 
-    # Waits until the action server has started up and started
-    # listening for goals.
-    client.wait_for_server()
+    def __init__(self):
+        # Creates the SimpleActionClient, passing the type of the action
+        self.client = actionlib.SimpleActionClient(
+            ACTION_SERVER_NAME,
+            fetch_pbd_interaction.msg.ExecuteAction
+        )
+        # Waits until the action server has started up and started
+        # listening for goals.
+        self.client.wait_for_server()
 
-    # Creates a goal to send to the action server.
-    action_goal = fetch_pbd_interaction.msg.ExecuteActionGoal()
-    action_goal.goal.action_names = action_names = [
-        'small_wave_hello',
-        'large_wave_hello',
-        'selfie_engage',
-        'selfie_disengage'
-    ]
-    action_goal.goal.continue_on_failure = False
+    def set_goal(self, action_names, continue_on_failure = False):
+        # Creates a goal to send to the action server.
+        self.action_goal = fetch_pbd_interaction.msg.ExecuteActionGoal().goal
+        self.action_goal.action_names = action_names
+        self.action_goal.continue_on_failure = continue_on_failure
 
-    # Sends the goal to the action server.
-    client.send_goal(action_goal.goal)
+    def send_goal(self):
+        # Sends the goal to the action server.
+        self.client.send_goal(self.action_goal, done_cb=self.done_cb, feedback_cb=self.feedback_cb)
+        # Waits for the server to finish performing the action.
+        self.client.wait_for_result()
 
-    # Waits for the server to finish performing the action.
-    client.wait_for_result()
+    def feedback_cb(self, feedback):
+        print("Feedback.\n{0}".format(str(feedback)))
 
-    # Prints out the result of executing the action
-    return client.get_result()
+    def done_cb(self, terminal_state, result):
+        print("Result.\n{0}".format(str(result)))
+
 
 # ######################################################################
 # Main Function
@@ -62,7 +63,13 @@ if __name__ == '__main__':
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
         rospy.init_node('fetch_pbd_action_client')
-        result = PBDClient()
-        print("Result.\n{0}".format(str(result)))
+        client = PBDClient()
+        action_names = [
+            'small_wave_hello',
+            'selfie_engage',
+            'selfie_disengage'
+        ]
+        client.set_goal(action_names)
+        client.send_goal()
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
